4994d39ed74166a6dc2e5f8b66771b82214eb195
tubi55 <tubi55@nate.com>
2023-01-24 15:10:36 +0900

    loading videos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const banner = document.querySelector('.banner');
 const list = banner.querySelector('ul');
 const tits = document.querySelectorAll('.tits h2');
-const vids = document.querySelectorAll('.bgs video');
+const bgFrame = document.querySelector('.bgs');
 const paging = document.querySelector('.paging');
+const mask = document.querySelector('.mask');
 const [btnPrev, btnNext] = document.querySelector('.btns').children;
 const [btnPlay, btnPause] = document.querySelector('.auto').children;
 const [counter, total] = paging.children;
+const vidData = ['vid1.mp4', 'vid2.mp4', 'vid3.mp4', 'vid4.mp4', 'vid5.mp4'];
 const showNum = 3;
 const speed = 500;
 const interval = 3000;
@@ -13,9 +15,11 @@ const len = list.children.length;
 let enableClick = true;
 let current_num = 0;
 let timer = null;
+let vids = null;
+let vid_count = 0;
 
 init();
-
+createVid();
 setTimeout(startRolling, interval);
 
 btnNext.addEventListener('click', () => {
@@ -35,6 +39,31 @@ function init() {
 	list.style.left = -100 / showNum + '%';
 	list.prepend(list.lastElementChild);
 	total.innerText = len < 10 ? '0' + len : len;
+}
+
+function createVid() {
+	let tags = '';
+	vidData.forEach((vid) => (tags += `<video src='vids/${vid}' loop muted autoplay></video>`));
+	bgFrame.innerHTML = tags;
+
+	vids = bgFrame.querySelectorAll('video');
+	vids.forEach((vid) => {
+		vid.onloadeddata = () => {
+			vid_count++;
+			console.log(vid_count);
+
+			if (vid_count === vidData.length) {
+				new Anime(mask, {
+					prop: 'opacity',
+					value: 0,
+					duration: 1000,
+					callback: () => mask.remove(),
+				});
+			}
+		};
+	});
+
+	vids[0].classList.add('on');
 }
 
 function next() {
