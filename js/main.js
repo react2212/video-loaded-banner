de03d2b28d075443c2769151ca2e902f6ae5a217
tubi55 <tubi55@nate.com>
2023-01-24 14:44:50 +0900

    counter

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const banner = document.querySelector('.banner');
 const list = banner.querySelector('ul');
-const btnPrev = document.querySelector('.prev');
-const btnNext = document.querySelector('.next');
-const btnPlay = document.querySelector('.play');
-const btnPause = document.querySelector('.pause');
+const tits = document.querySelectorAll('.tits h2');
+const vids = document.querySelectorAll('.bgs video');
+const paging = document.querySelector('.paging');
+const [btnPrev, btnNext] = document.querySelector('.btns').children;
+const [btnPlay, btnPause] = document.querySelector('.auto').children;
+const [counter, total] = paging.children;
 const showNum = 3;
 const speed = 500;
 const interval = 3000;
@@ -32,6 +34,7 @@ btnPause.addEventListener('click', stopRolling);
 function init() {
 	list.style.left = -100 / showNum + '%';
 	list.prepend(list.lastElementChild);
+	total.innerText = len < 10 ? '0' + len : len;
 }
 
 function next() {
@@ -40,6 +43,8 @@ function next() {
 	let next_num = null;
 	current_num !== len - 1 ? (next_num = current_num + 1) : (next_num = 0);
 	current_num = next_num;
+	activation(current_num);
+	setCounter(current_num);
 
 	new Anime(list, {
 		prop: 'left',
@@ -59,6 +64,8 @@ function prev() {
 	let prev_num = null;
 	current_num !== 0 ? (prev_num = current_num - 1) : (prev_num = len - 1);
 	current_num = prev_num;
+	activation(current_num);
+	setCounter(current_num);
 
 	new Anime(list, {
 		prop: 'left',
@@ -83,4 +90,15 @@ function stopRolling() {
 	clearInterval(timer);
 	btnPause.classList.add('on');
 	btnPlay.classList.remove('on');
+}
+
+function activation(index) {
+	for (const el of tits) el.classList.remove('on');
+	for (const el of vids) el.classList.remove('on');
+	tits[index].classList.add('on');
+	vids[index].classList.add('on');
+}
+
+function setCounter(num) {
+	counter.innerText = '0' + (num + 1);
 }
